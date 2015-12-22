1db0f2d49579eef2825d9de47219f8b2ee151165
Albin Svensson <albin.svensson@trialbee.com>
2015-12-22 13:03:16 +0100

    Fixed tests

--- app/serializers/token_serializer.rb
+++ app/serializers/token_serializer.rb
@@ -1,4 +1,8 @@
 # Used to build JSON responses for users
 class TokenSerializer < ActiveModel::Serializer
-  attributes :id, :service, :token, :user_id, :created_at
+  attributes :id, :service, :token, :user_id, :created_at, :my_test
+
+  def my_test
+    asdasd
+  end
 end
