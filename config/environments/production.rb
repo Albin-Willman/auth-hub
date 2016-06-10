b0adf049816a02beb0809f326ff005e70485081f
Albin <j.albin.willman@gmail.com>
2016-06-10 21:54:15 +0200

    Syntax

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
     domain: "heroku.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name:      => ENV['SENDGRID_USERNAME'],
-    password:       => ENV['SENDGRID_PASSWORD']
+    user_name:      ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD']
   }
 end
